d490740cdb993fdb6de3bcfc1a4fbdc1d9ed2eed
masterUNG <phrombutr@gmail.com>
2018-12-03 06:41:36 +0700

    update 3dec

---
+++ node/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', GET, POST, PUT, DELETE);
+    res.setHeader('Access-Control-Allow-Headers', 'content-type, x-access-token');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+});
+
+
+app.get('/', (req, res)=>{
+    res.end('This is root path');
+});
+
+app.get('/home', (req, res)=>{
+    res.end('This is root home');
+});
+
+app.post('/api', (req, res)=>{
+    const userName = req.body.userName;
+    const passWord = req.body.passWord;
+    res.end('MasterUNG Feedback => userName: ' + userName + ', passWord: ' + passWord);
+});
+
+app.listen(3000, ()=>{
+    console.log('Server Work...');
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'TestHello';
+
+  constructor(private http:HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    
+    let data = {userName: 'Doramon', passWord: 'dora1234'}
+    this.http.post<any>('http://localhost:3000/api', data).subscribe(result=>{
+      alert(JSON.stringify(result));
+    });
+
+  }
+
 }
+
+
