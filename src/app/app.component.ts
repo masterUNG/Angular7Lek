e15a43f6e54b339a6f0ca8a493d3eff6bd6470f7
masterUNG <phrombutr@gmail.com>
2019-02-19 08:23:28 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,33 +8,18 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
   title = 'TestHello';
-
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
     console.log("onOninit Work");
-    // let data = {userName: 'Doramon123', passWord: 'dora1234'}
-    // this.http.post<any>('http://localhost:3000/api', data).subscribe(result=>{
-    //   alert(JSON.stringify(result));
-    // });
-
   }
 
-  onSubmit(userName: string) {
-
-    console.log('userName ==> ' + userName);
-
-    let data = {userName: userName, passWord: 'dora1234'};
-
-    console.log('data ==> ' + JSON.stringify(data));
-
-    alert(JSON.stringify(data));
-    this.http.post<any>('http://localhost:3000/api', data).subscribe(result => {
-      alert(JSON.stringify(result));
-    });
+  onSubmit(feedBackForm: NgForm) {
+    
+    console.log('userName ==> ' + feedBackForm.value.userName);
+    
   }
 
 
